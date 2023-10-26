4ef8fa6afb520cb2687723baaa42a120b497f583
ayan <ayan.d3v@gmail.com>
2023-10-26 12:07:55 +0600

    smtj

--- server/db.js
+++ server/db.js
@@ -7,6 +7,13 @@ module.exports = new Sequelize (
     {
         dialect: 'postgres',
         host: process.env.DB_HOST,
-        port: process.env.DB_PORT
+        port: process.env.DB_PORT,
+        ssl: true,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
     }
 )
